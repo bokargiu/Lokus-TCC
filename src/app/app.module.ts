949bdb6f1d9978dfcdab2e966975546d596fd056
bokargiu <erickjstaack07@gmail.com>
2025-06-26 10:08:56 -0300

    Cdk-Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CdkMenuModule } from '@angular/cdk/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +15,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    CdkMenuModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
